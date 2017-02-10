4194c5071a40232d04ce8752b7f9c51d03daacc2
Thomas <Thomas@DevBook>
2017-02-10 21:13:42 +0100

    removed comment

--- super-simple-only-npm-scripts/public/app.jsx
+++ super-simple-only-npm-scripts/public/app.jsx
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Header } from "./header";
 
-// Filterable CheatSheet Component
 ReactDOM.render(<Header text="Hello React!" />,
     document.getElementById('root'));
