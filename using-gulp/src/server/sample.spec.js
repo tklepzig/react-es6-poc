7eb81e543a585920dbc66aaa141c38eae53a14a0
Thomas <Thomas@DevBook>
2017-06-17 20:47:13 +0200

    added testing with mocha, chai and sinon

---
+++ using-gulp/src/server/sample.spec.js
@@ -0,0 +1,10 @@
+import chai from "chai";
+import sinon from "sinon";
+
+const expect = chai.expect;
+
+describe("Test", () => {
+    it("should work", () => {
+        expect(true).to.be.true;
+    });
+});
