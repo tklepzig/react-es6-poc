0dbc229c6355de6d22ef0cadcf1da9ba05af4bc2
Thomas <Thomas@DevBook>
2017-07-09 12:52:52 +0200

    components filename PascalCase

--- using-gulp/src/public/app.jsx
+++ using-gulp/src/public/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "~/header";
+import Header from "~/Header";
 
 ReactDOM.render(<Header text="Hello React!" />,
     document.getElementById("root"));
