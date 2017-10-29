7a7ca43ff32877257424cbd5eef678f14a636b9a
Thomas <Thomas@DevBook>
2017-10-29 19:56:28 +0100

    added dev scripts

--- typescript-webpack/src/public/app.tsx
+++ typescript-webpack/src/public/app.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Header } from "./components/Header";
 
-ReactDOM.render(<Header text="Hello React2!" />,
+ReactDOM.render(<Header text="Hello React!" />,
     document.getElementById("root"));
