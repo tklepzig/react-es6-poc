f19addcbcbf9a75247c85886c9d7a4b90dac723d
Thomas <Thomas@DevDisc>
2017-10-31 14:25:59 +0100

    modified sample test

--- typescript-webpack/src/public/components/Header.spec.tsx
+++ typescript-webpack/src/public/components/Header.spec.tsx
@@ -3,8 +3,15 @@ import { mount, configure, shallow } from "enzyme";
 import * as React from "react";
 
 describe("Header", () => {
-    it("should work", () => {
-        const header = mount(<Header text="Test" />);
-        expect(header.find("h1").text()).toBe("Test");
+    const header = mount(<Header text="Test" />);
+
+    it("should have h1", () => {
+        expect(header.find("h1").length).toBe(1);
     });
+
+    it("should have correct title", () => {
+        expect(header.find("h1").text()).toBe("Test");
+
+    });
+
 });
