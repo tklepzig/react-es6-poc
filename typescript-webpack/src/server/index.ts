4f019adbf1a71bc3cbf78b4e7c511dbb860dae4a
Thomas Klepzig <thomas.klepzig@comparex.de>
2017-11-13 11:48:30 +0100

    some updates

--- typescript-webpack/src/server/index.ts
+++ typescript-webpack/src/server/index.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
 import * as path from "path";
 
+const port = 8080;
 const app = express();
-
-console.log(path.resolve(`${__dirname}`));
 
 app.use("/", express.static(path.resolve(`${__dirname}/../public`)));
 
-app.listen(8080, function () {
-    console.log("listening on *:8080");
+app.listen(port, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`listening on *:${port}`);
 });
