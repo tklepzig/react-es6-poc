e8bde5303ab243056575459576d0ccbda02614a1
Thomas <Thomas@DevBook>
2018-11-24 18:33:45 +0100

    Remove unnecessary env setting (is done by webpack in prod mode)

--- typescript-webpack/webpack.prod.js
+++ typescript-webpack/webpack.prod.js
@@ -1,4 +1,3 @@
-const webpack = require("webpack");
 const merge = require("webpack-merge");
 const TerserPlugin = require('terser-webpack-plugin');
 const common = require("./webpack.common");
@@ -12,11 +11,6 @@ const server = {
 
 const client = {
     mode: "production",
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": { "NODE_ENV": JSON.stringify("production") }
-        })
-    ],
     optimization: {
         minimizer: [new TerserPlugin()]
     }
